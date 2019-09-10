e7128a9b137a78bdecf382c98abde1cd84f02c6a
mittulmandhan <conanmittul@gmail.com>
2019-09-10 13:50:37 +0530

    class5: sending response to client with status code

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+// we are creating a simple javascript array instead of requesting data from database for simplicity
+// we will use this array to perform CRUD operations
+let users = [{ id: 1, name: 'Mohan', address: 'Noida', contact: '9778854321' }]
+
+router.get('/users', function(req, res, next) {
+    //we are sending data in json format in response
+    // we can also send status code as well as data response using following command
+    //By default status code is  200 but we can send other codes as well
+    res.status(HttpStatus.OK).json(users);
 });
 
 module.exports = router;

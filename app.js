c51dfe3f891e0399b2cb4d1c7035f623cf9ff762
mittulmandhan <conanmittul@gmail.com>
2019-09-11 05:39:55 +0530

    cors installed to handle cross domain access error and added as dependency

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var logger = require('morgan');
 var apiRouter = require('./routes/api');
+var cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
